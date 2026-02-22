057c2789506c0b1c7c3a45d50bea45ed8ca93c06
Divya-teeda <145985576+Divya-teeda@users.noreply.github.com>
2026-02-22 22:47:56 +0530

    Fixed the 404

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
 const repoName = 'Myportfolio';
 
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  trailingSlash: true,
-  basePath: isGithubPages ? `/${repoName}` : '',
-  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
